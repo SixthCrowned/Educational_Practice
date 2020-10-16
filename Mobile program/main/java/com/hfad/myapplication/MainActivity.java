55775a911c62ee9cfd9b823ea68d96dc397e1e54
SixthCrowned <66189109+SixthCrowned@users.noreply.github.com>
2020-10-16 17:37:24 +0300

    Add files via upload

---
+++ Mobile program/main/java/com/hfad/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.hfad.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openNewActivity(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
